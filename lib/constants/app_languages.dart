3e9b5588d4d10cc6223da9e283daf4bdd7907955
Jeff McMorris <delay@Jeffs-MacBook-Pro.local>
2020-04-15 15:49:43 -0500

    minor change

--- lib/constants/app_languages.dart
+++ lib/constants/app_languages.dart
@@ -3,7 +3,6 @@ import 'package:flutter_starter/models/models.dart';
 class AppLanguages {
   //AppLanguages._();
   static final List<LanguageModel> languageOptions = [
-    LanguageModel(key: "Blank", value: ""),
     LanguageModel(key: "中文", value: "zh"), //Chinese
     LanguageModel(key: "Deutsche", value: "de"), //German
     LanguageModel(key: "English", value: "en"), //English
